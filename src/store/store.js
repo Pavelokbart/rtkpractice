aebfbd41878222164cf83394c705da50012cd7df
Pavel Bartos <pbartos03@mail.ru>
2024-09-17 21:46:35 +0300

    add redux

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { reducer } from "./favorites/favorites.slice";
 
 export const store = configureStore(
     {
-        reducer:{},
-        
+        reducer,
+
     }
 )
